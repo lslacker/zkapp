a63cf35ba4ad8f55168513d1df2497064a8d0cdf
Luan Mai <mai.luan@gmail.com>
2020-10-10 10:30:57 +1100

    Initial import

---
+++ src/main/java/info/thatngo/zkapp/ZkappApplication.java
@@ -0,0 +1,13 @@
+package info.thatngo.zkapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZkappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZkappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/info/thatngo/zkapp/ZkappApplicationTests.java
@@ -0,0 +1,13 @@
+package info.thatngo.zkapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZkappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
